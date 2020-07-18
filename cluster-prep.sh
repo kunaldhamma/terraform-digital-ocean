fd2961276aea299d553f993f5210527d0c30c182
jamesbuckett <james.buckett@gmail.com>
2020-07-18 09:03:45 +0800

    Added support files

---
+++ cluster-prep.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# doctl
+doctl auth init --access-token "92be07eeeb8854eb01ee52fdc9f931aeb8fa4d3b1a8a7ff4546e460eddc972df"
+doctl kubernetes cluster kubeconfig save digital-ocean-cluster
+
+kubectl config use-context do-sgp1-digital-ocean-cluster
+
+# metrics server
+kubectl create namespace ns-metrics-server
+kubectl apply -f "https://raw.githubusercontent.com/jamesbuckett/kubernetes-tools/master/components.yaml"
+
+# Contour
+kubectl create namespace ns-contour
+helm upgrade --install contour-release stable/contour --namespace ns-contour --set service.loadBalancerType=LoadBalancer
+
+# Loki
+kubectl create ns ns-loki
+helm repo remove loki
+helm repo add loki https://grafana.github.io/loki/charts
+helm repo update
+helm upgrade --install loki-release loki/loki-stack -f  "https://raw.githubusercontent.com/jamesbuckett/microservices-metrics-docker-desktop/master/values.yaml" -n ns-loki
+
+# Online Boutique
+kubectl create namespace ns-microservices-demo
+kubectl apply -n ns-microservices-demo -f "https://raw.githubusercontent.com/jamesbuckett/microservices-metrics-chaos/master/complete-demo.yaml"
+
+# Locust
+cd ~/ && rm -R ~/locust
+cd ~/ && mkdir locust && cd locust
+wget https://raw.githubusercontent.com/jamesbuckett/microservices-metrics-chaos/master/locustfile.py
+
+# Gremlin
+helm repo remove gremlin
+helm repo add gremlin https://helm.gremlin.com
+
+
+# Update .bashrc
+cd ~
+echo "source <(kubectl completion bash)" >>~/.bashrc
+echo "alias cls='clear'" >> ~/.bashrc
+echo "alias k='kubectl'" >> ~/.bashrc
+echo "alias kga='kubectl get all'" >> ~/.bashrc
+echo "KUBE_PS1_SYMBOL_ENABLE=false" >>~/.bashrc
+echo "source /opt/kube-ps1/kube-ps1.sh" >>~/.bashrc
+echo "export OCTANT_ACCEPTED_HOSTS=$DROPLET_ADDR" >> ~/.bashrc
+echo "export OCTANT_DISABLE_OPEN_BROWSER=1" >> ~/.bashrc
+echo "export OCTANT_LISTENER_ADDR=0.0.0.0:8900" >> ~/.bashrc
+
+echo "Octant is here: " 
+doctl compute droplet list | awk 'FNR == 2 {print $3}'
+echo "Port 8900"
+
+echo "Online Boutique is here: "
+kubectl -n ns-contour get service contour-release | awk 'FNR == 2 {print $4}
+echo "No Port"
+
+# Start Locust
+
+#FRONTEND_ADDR=$(kubectl -n ns-contour get service contour-release | awk 'FNR == 2 {print $4}')
+#export $FRONTEND_ADDR
+#~/locust/locust --host="http://${FRONTEND_ADDR}" -u "${USERS:-10}" &
+# echo "Locust is here: "
+# doctl compute droplet list | awk 'FNR == 2 {print $3}'
+# echo "Port 8089"
+
+# Start Octant 
+
+# DROPLET_ADDR=$(doctl compute droplet list | awk 'FNR == 2 {print $3}')
+# export $DROPLET_ADDR
+# octant & 
+
+#echo "PS1='[\u@\h \w $(kube_ps1)]\$ '" >>~/.bashrc
+
+#End of Script
---
+++ jump-host-prep.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Preparation
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt install -y python3-pip -y
+
+# doctl
+cd ~/ && rm -R ~/doctl
+cd ~/ && mkdir doctl && cd doctl
+curl -LO https://github.com/digitalocean/doctl/releases/download/v1.45.1/doctl-1.45.1-linux-amd64.tar.gz 
+tar -xvf doctl-1.45.1-linux-amd64.tar.gz
+sudo mv ~/doctl/doctl /usr/local/bin
+
+# kubectl 
+cd ~/ && rm -R ~/kubectl
+cd ~/ && mkdir kubectl && cd kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+# kubectx & kubens
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
+# kube-ps1
+sudo git clone https://github.com/jonmosco/kube-ps1.git /opt/kube-ps1
+
+# Helm 3
+cd ~/ && rm -R ~/helm-3
+cd ~/ && mkdir helm-3 && cd helm-3
+wget https://get.helm.sh/helm-v3.2.4-linux-amd64.tar.gz
+tar -zxvf helm-v3.2.4-linux-amd64.tar.gz
+sudo mv linux-amd64/helm /usr/local/bin/helm
+helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+
+# Octant
+cd ~/ && rm -R ~/octant
+cd ~/ && mkdir octant && cd octant
+curl -LO https://github.com/vmware-tanzu/octant/releases/download/v0.13.1/octant_0.13.1_Linux-64bit.tar.gz
+tar -xvf octant_0.13.1_Linux-64bit.tar.gz
+sudo mv ./octant_0.13.1_Linux-64bit/octant /usr/local/bin/octant
+
+# Locust
+pip3 install locust
+cd ~/ && rm -R ~/locust
+cd ~/ && mkdir locust && cd locust
+wget https://raw.githubusercontent.com/jamesbuckett/microservices-metrics-chaos/master/locustfile.py
+
+reboot
+
+# End of Script
