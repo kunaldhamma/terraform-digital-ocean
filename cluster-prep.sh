a5883b57a11fbefee474f002f223a18e7d638fb9
jamesbuckett <james.buckett@gmail.com>
2020-09-11 10:27:09 +0800

    Message of the Day updated

--- cluster-prep.sh
+++ cluster-prep.sh
@@ -58,14 +58,15 @@ echo "export OCTANT_LISTENER_ADDR=0.0.0.0:8900" >> ~/.bashrc
 
 # Update Message of the Day
 echo "Reference commands to the various URLs in this tutorial" >> /etc/motd
-echo "****************************************************" >> /etc/motd
-echo "* Online Boutique is here: echo $BOUTIQUE_LB       *" >> /etc/motd
-echo "* Octant is here: echo $DROPLET_ADDR:8900          *" >> /etc/motd
-echo "* Grafana is here: echo $GRAFANA_LB                *" >> /etc/motd
-echo "* Locust is here: echo $DROPLET_ADDR:8089          *" >> /etc/motd
-echo "****************************************************"  >> /etc/motd
-
-printf "%s\n" "Add this to .bashrc manually 'PS1='[\u@\h \w $(kube_ps1)]\$ '"
+echo "*************************************************************************************" >> /etc/motd
+echo "* Online Boutique is here: echo $BOUTIQUE_LB                                        *" >> /etc/motd
+echo "* Octant is here: echo $DROPLET_ADDR:8900                                           *" >> /etc/motd
+echo "* Grafana is here: echo $GRAFANA_LB                                                 *" >> /etc/motd
+echo "* Locust is here: echo $DROPLET_ADDR:8089                                           *" >> /etc/motd
+echo "* Start in another shell : octant &                                                 *" >> /etc/motd
+echo "* Start in another shell: locust --host="http://${BOUTIQUE_LB}" -u "${USERS:-10}" & *" >> /etc/motd
+echo "* Add this to .bashrc manually 'PS1='[\u@\h \w $(kube_ps1)]\$ '                     *" >> /etc/motd
+echo "*************************************************************************************" >> /etc/motd
 
 reboot
 
