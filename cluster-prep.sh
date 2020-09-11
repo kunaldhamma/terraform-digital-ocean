9d9604850a06f9a0cfe2bd1274efd4985b10bba7
jamesbuckett <james.buckett@gmail.com>
2020-09-11 15:50:35 +0800

    Added online-boutique.sh to accont for delayed LB build

--- cluster-prep.sh
+++ cluster-prep.sh
@@ -39,8 +39,8 @@ DROPLET_ADDR=$(doctl compute droplet list | awk 'FNR == 2 {print $3}')
 export DROPLET_ADDR
 
 # Online Boutique
-BOUTIQUE_LB=$(doctl compute load-balancer list | awk 'FNR == 2 {print $2}')
-export BOUTIQUE_LB
+# BOUTIQUE_LB=$(doctl compute load-balancer list | awk 'FNR == 2 {print $2}')
+# export BOUTIQUE_LB
 
 # Update .bashrc
 cd ~
@@ -50,23 +50,11 @@ echo "alias k='kubectl'" >> ~/.bashrc
 echo "alias kga='kubectl get all'" >> ~/.bashrc
 echo "KUBE_PS1_SYMBOL_ENABLE=false" >>~/.bashrc
 echo "source /opt/kube-ps1/kube-ps1.sh" >>~/.bashrc
-echo "export BOUTIQUE_LB=$BOUTIQUE_LB" >> ~/.bashrc
+# echo "export BOUTIQUE_LB=$BOUTIQUE_LB" >> ~/.bashrc
 echo "export DROPLET_ADDR=$DROPLET_ADDR" >> ~/.bashrc
 echo "export OCTANT_ACCEPTED_HOSTS=$DROPLET_ADDR" >> ~/.bashrc
 echo "export OCTANT_DISABLE_OPEN_BROWSER=1" >> ~/.bashrc
 echo "export OCTANT_LISTENER_ADDR=0.0.0.0:8900" >> ~/.bashrc
-
-# Update Message of the Day
-echo "Reference commands to the various URLs in this tutorial" >> /etc/motd
-echo "*************************************************************************************" >> /etc/motd
-echo "* Online Boutique is here: echo $BOUTIQUE_LB                                        *" >> /etc/motd
-echo "* Octant is here: echo $DROPLET_ADDR:8900                                           *" >> /etc/motd
-echo "* Grafana is here: echo $GRAFANA_LB                                                 *" >> /etc/motd
-echo "* Locust is here: echo $DROPLET_ADDR:8089                                           *" >> /etc/motd
-echo "* Start in another shell : octant &                                                 *" >> /etc/motd
-echo "* Start in another shell: locust --host="http://${BOUTIQUE_LB}" -u "${USERS:-10}" & *" >> /etc/motd
-echo "* Add this to .bashrc manually 'PS1='[\u@\h \w $(kube_ps1)]\$ '                     *" >> /etc/motd
-echo "*************************************************************************************" >> /etc/motd
 
 reboot
 
