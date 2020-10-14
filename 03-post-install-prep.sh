983de43d68af66f86bb4b873e4dd1782c46ecc83
jamesbuckett <james.buckett@gmail.com>
2020-10-14 11:58:43 +0800

    Fixed Ordering of LB allocation

--- 03-post-install-prep.sh
+++ 03-post-install-prep.sh
@@ -11,10 +11,6 @@ sleep 4m
 BOUTIQUE_LB=$(doctl compute load-balancer list | awk 'FNR == 2 {print $2}')
 export BOUTIQUE_LB
 
-# Goldilocks - Export the Public IP address of Golidilocks 
-GOLDILOCKS_LB=$(doctl compute load-balancer list | awk 'FNR == 3 {print $2}')
-export GOLDILOCKS_LB
-
 # Loki - Export the Public IP address of Loki and display the password to login
 LOKI_LB=$(doctl compute load-balancer list | awk 'FNR == 4 {print $2}')
 export LOKI_LB
@@ -27,6 +23,10 @@ export CHAOSMESH_LB
 # Scale deployment.apps/frontend to 3 replicas for Chaos Experiments 
 kubectl scale deployment.apps/frontend --replicas=3 -n ns-microservices-demo
 
+# Goldilocks - Export the Public IP address of Golidilocks 
+GOLDILOCKS_LB=$(doctl compute load-balancer list | awk 'FNR == 3 {print $2}')
+export GOLDILOCKS_LB
+
 # Update .bashrc
 cd ~
 echo "export BOUTIQUE_LB=$BOUTIQUE_LB" >> ~/.bashrc
@@ -37,13 +37,13 @@ echo "export CHAOSMESH_LB=$CHAOSMESH_LB" >> ~/.bashrc
 # Update Message of the Day
 echo "Reference URLs in this tutorial" >> /etc/motd
 echo "**********************************************************************************************" >> /etc/motd
-echo "* Sample Microservices Application - Online Boutique is here: $BOUTIQUE_LB " >> /etc/motd
-echo "* Real-time Kubernetes Dashboard - Octant is here:  $DROPLET_ADDR:8900 " >> /etc/motd
-echo "* Vertical Pod Autoscaler recommendations - Goldilocks is here: $GOLDILOCKS_LB " >> /etc/motd
-echo "* Distributed Log Aggregation - Loki is here: $LOKI_LB  " >> /etc/motd
+echo "* Sample Microservices Application - Online Boutique is here: $BOUTIQUE_LB.xip.io " >> /etc/motd
+echo "* Real-time Kubernetes Dashboard - Octant is here:  $DROPLET_ADDR:8900.xip.io " >> /etc/motd
+echo "* Vertical Pod Autoscaler recommendations - Goldilocks is here: $GOLDILOCKS_LB.xip.io " >> /etc/motd
+echo "* Distributed Log Aggregation - Loki is here: $LOKI_LB.xip.io  " >> /etc/motd
 echo "* Loki User:  admin   Loki Password: $LOKI_PWD"
-echo "* Chaos Engineering Platfom - Chaos Mesh  is here: $CHAOSMESH_LB:2333  " >> /etc/motd
-echo "* Load Testing Tool - Locust is here: $DROPLET_ADDR:8089 " >> /etc/motd
+echo "* Chaos Engineering Platfom - Chaos Mesh  is here: $CHAOSMESH_LB:2333.xip.io  " >> /etc/motd
+echo "* Load Testing Tool - Locust is here: $DROPLET_ADDR:8089.xip.io " >> /etc/motd
 echo "* Locust values are Spawn:500 & URL: $BOUTIQUE_LB " >> /etc/motd                      
 echo "* Start Locust & Octant in another shell : sh /root/locust/startup-locust.sh " >> /etc/motd      
 #echo "* Add this to .bashrc manually 'PS1='[\u@\h \w $(kube_ps1)]\$ '                             *" >> /etc/motd
