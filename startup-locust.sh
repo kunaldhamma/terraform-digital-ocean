84aa489b94616c2833fb8a91cad614e65c57b970
jamesbuckett <james.buckett@gmail.com>
2020-10-14 10:43:17 +0800

    Updated with Comments

--- startup-locust.sh
+++ startup-locust.sh
@@ -1,6 +1,12 @@
+# Author:  James Buckett
+# eMail: james.buckett@gmail.com
+# Script to start Locust and Octant
+
 #!/bin/bash
 
+# Start Locust in a background process with Public IP and Users populated
 cd /root/locust
 locust --host="http://${BOUTIQUE_LB}" -u "${USERS:-10}" &
 
+# Start Octant in a background process 
 octant & 
