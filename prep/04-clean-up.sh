fe9b82d9bb6f7c263350d0d69f3ccd3421b09521
jamesbuckett <james.buckett@gmail.com>
2020-10-19 13:26:15 +0800

    Changed the clean up order to prevent hang state

--- prep/04-clean-up.sh
+++ prep/04-clean-up.sh
@@ -14,18 +14,6 @@ doctl kubernetes cluster delete digital-ocean-cluster -f
 
 printf "%s\n" "digital-ocean-cluster deleted"
 
-# Virtual Machine
-
-doctl compute droplet delete digital-ocean-droplet -f
-
-printf "%s\n" "digital-ocean-droplet deleted"
-
-# Volumes
-
-doctl compute volume list | awk 'FNR == 2 {print $1}' | xargs doctl compute volume delete -f
-
-printf "%s\n" "digital-ocean-droplet volume deleted"
-
 # Load Balancers
 
 # Online Boutique
@@ -41,5 +29,17 @@ doctl compute load-balancer list | awk 'FNR == 2 {print $1}' | xargs doctl compu
 doctl compute load-balancer list | awk 'FNR == 2 {print $1}' | xargs doctl compute load-balancer delete -f
 
 printf "%s\n" "digital-ocean-loadbalancers deleted"
+
+# Virtual Machine
+
+# Volumes
+
+doctl compute volume list | awk 'FNR == 2 {print $1}' | xargs doctl compute volume delete -f
+
+printf "%s\n" "digital-ocean-droplet volume deleted"
+
+doctl compute droplet delete digital-ocean-droplet -f
+
+printf "%s\n" "digital-ocean-droplet deleted"
 
 printf "%s\n" "Done with clean up on Digital Ocean...."
