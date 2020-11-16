f63d4dd5364ebe0f295676ae703978c8e67c7570
jamesbuckett <james.buckett@gmail.com>
2020-11-16 14:41:08 +0800

    Updated with knative install steps

--- under-development/05-knative-install.sh
+++ under-development/05-knative-install.sh
@@ -2,26 +2,56 @@
 # eMail: james.buckett@gmail.com
 # Script to install Knative
 
+# Create your first Knative app
+# Link : https://opensource.com/article/20/11/knative
+
 #!/bin/bash
 
+# Check that you are on jump host and not local host
+if [ "$HOSTNAME" = "digital-ocean-droplet" ]; then
+
+# Knative CLI install
+clear
+echo "Installing Knative CLI..."
+cd ~/ && rm -R ~/knative
+cd ~/ && mkdir knative && cd knative
+git clone https://github.com/knative/client.git
+cd knative/client/hack
+./build.sh -f
+sudo cp kn /usr/local/bin
+echo "kn version"
+sleep 5
+
 # Knative Installation
+echo "Installing Knative Serving..."
+export KNATIVE="0.17.2"
+kubectl delete namespace knative-serving
+clear
+kubectl create namespace knative-serving
+kubectl apply -f https://github.com/knative/serving/releases/download/v$KNATIVE/serving-crds.yaml
+kubectl apply -f https://github.com/knative/serving/releases/download/v$KNATIVE/serving-core.yaml
+kubectl wait -n knative-serving deploy controller --for condition=Available --timeout=90s
+echo "Knative serving installed...."
+sleep 5
 
-helm repo add istio.io https://storage.googleapis.com/istio-release/releases/1.5.4/charts/
-kubectl create namespace istio-system
-helm template install/kubernetes/helm/istio-init --name istio-init --namespace istio-system | kubectl apply -f -
-helm template install/kubernetes/helm/istio --name istio --namespace istio-system \
-    --values install/kubernetes/helm/istio/values-istio-demo.yaml | kubectl apply -f -
+# Kourier Installation
+echo "Installing Kourier..."
+export KOURIER="0.17.0"
+kubectl delete namespace kourier-system
+kubectl create namespace kourier-system
+kubectl apply -f https://github.com/knative/net-kourier/releases/download/v$KOURIER/kourier.yaml
+kubectl wait -n kourier-system deploy 3scale-kourier-gateway --for condition=Available --timeout=90s
 
-kubectl apply --selector knative.dev/crd-install=true \
---filename https://github.com/knative/serving/releases/download/v0.11.0/serving.yaml \
---filename https://github.com/knative/eventing/releases/download/v0.11.0/release.yaml \
---filename https://github.com/knative/serving/releases/download/v0.11.0/monitoring.yaml
+kubectl patch configmap/config-network \
+  --namespace knative-serving \
+  --type merge \
+  --patch '{"data":{"ingress.class":"kourier.ingress.networking.knative.dev"}}'
+echo "Kourier installed and patched..."
+sleep 5
 
-kubectl apply \
---filename https://github.com/knative/serving/releases/download/v0.11.0/serving.yaml \
---filename https://github.com/knative/eventing/releases/download/v0.11.0/release.yaml \
---filename https://github.com/knative/serving/releases/download/v0.11.0/monitoring.yaml
+kubectl get service kourier -n kourier-system
 
-kubectl get pods --namespace knative-serving
-kubectl get pods --namespace knative-eventing
-kubectl get pods --namespace knative-monitoring
+else
+    echo "You are not on the jump host : digital-ocean-droplet"
+    exit
+fi
