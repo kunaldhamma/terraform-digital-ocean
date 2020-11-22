d54c49594682aae92f7d13fc2ada88566244f641
jamesbuckett <james.buckett@gmail.com>
2020-11-22 09:47:38 +0800

    Fixed Install Sequence

--- prep/03-post-install-prep.sh
+++ prep/03-post-install-prep.sh
@@ -4,14 +4,18 @@
 
 #!/bin/bash
 
-# Stop the script on errors
-set -euo pipefail
-
 # Check that you are on jump host and not local host
 if [ "$HOSTNAME" = "digital-ocean-droplet" ]; then
 
+# Clear any previous installations
+cd ~/ && rm -R ~/locust
+
+# Stop the script on errors
+set -euo pipefail
+
 # Wait for the Load Balancers to  provision
 sleep 2m
+watch -n1 doctl compute load-balancer list
 
 # Online Boutique - Export the Public IP address of Online Boutique 
 BOUTIQUE_LB=$(doctl compute load-balancer list | awk 'FNR == 2 {print $2}')
@@ -35,6 +39,7 @@ export GOLDILOCKS_LB
 
 # Update .bashrc
 cd ~
+cp .bashrc .bashrc-original
 echo "export BOUTIQUE_LB=$BOUTIQUE_LB" >> ~/.bashrc
 echo "export GOLDILOCKS_LB=$GOLDILOCKS_LB" >> ~/.bashrc
 echo "export LOKI_LB=$LOKI_LB" >> ~/.bashrc
@@ -47,7 +52,7 @@ echo "**************************************************************************
 echo "* Real-time Kubernetes Dashboard - Octant is here:  $DROPLET_ADDR:8900 " >> /etc/motd
 echo "* Sample Microservices Application - Online Boutique is here: $BOUTIQUE_LB.xip.io " >> /etc/motd
 echo "* Distributed Log Aggregation - Loki is here: $LOKI_LB.xip.io  " >> /etc/motd
-echo "* Loki User:  admin   Loki Password: $LOKI_PWD"
+echo "* Loki User:  admin   Loki Password: $LOKI_PWD" >> /etc/motd
 echo "* Chaos Engineering Platfom - Chaos Mesh  is here: $CHAOSMESH_LB:2333  " >> /etc/motd
 echo "* Vertical Pod Autoscaler recommendations - Goldilocks is here: $GOLDILOCKS_LB.xip.io " >> /etc/motd
 echo "* Load Testing Tool - Locust is here: $DROPLET_ADDR:8089 " >> /etc/motd
@@ -57,7 +62,6 @@ echo "* Start Locust & Octant in another shell : sh /root/locust/startup-locust.
 echo "**********************************************************************************************" >> /etc/motd
 
 # Locust - Setup Locust
-cd ~/ && rm -R ~/locust
 cd ~/ && mkdir locust && cd locust
 wget https://raw.githubusercontent.com/jamesbuckett/microservices-metrics-chaos/master/locustfile.py
 wget https://raw.githubusercontent.com/jamesbuckett/terraform-digital-ocean/master/service/startup-locust.sh
