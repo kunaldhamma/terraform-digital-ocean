0e0feb4f12f91aaa70984c23a359ee46a618c727
jamesbuckett <james.buckett@gmail.com>
2021-05-18 22:12:34 +0800

    rm /usr/local/bin/kubectx

--- prep/01-jump-host-prep.sh
+++ prep/01-jump-host-prep.sh
@@ -20,6 +20,7 @@ cd ~/ && rm -R ~/helm-3
 cd ~/ && rm -R ~/octant
 cd ~/ && rm -R ~/argo
 rm -R /opt/kubectx
+rm /usr/local/bin/kubectx
 
 ################################################################################
 # Stop the script on errors
