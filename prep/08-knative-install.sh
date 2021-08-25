a3e7eddf87591d511bbe13d196b2d87bf6e445ab
James Buckett <18049790+jamesbuckett@users.noreply.github.com>
2021-08-25 14:45:57 +0800

    Update 08-knative-install.sh
    Worked Knative serving example

--- prep/08-knative-install.sh
+++ prep/08-knative-install.sh
@@ -38,20 +38,42 @@ doctl compute domain records create jamesbuckett.com --record-type CNAME --recor
 
 kubectl patch configmap/config-domain   --namespace knative-serving   --type merge   --patch '{"data":{"knative.jamesbuckett.com":""}}'
 
-## Hello World
+## Hello Example
 
+# Check clean slate
+# kn service list
+
+# Deploy first application passing environment variable=TARGET="First"
 # kn service create hello-example --image gcr.io/knative-samples/helloworld-go --env TARGET="First" -n ns-knative
 
+# Curl or Browser "First" deployment
+# curl http://hello-example.knative.jamesbuckett.com
+# Should be: "Hello First!"
+
+# Update Service by passing a new environment variable=TARGET=Second
 # kn service update hello-example --env TARGET=Second -n ns-knative
 
+# Both revsions exist aka "First" and Second
 # kn revision list
 
-# kn revision describe 
+# Curl or Browser Second deployment
+# curl http://hello-example.knative.jamesbuckett.com
+# Should be: "Hello Second!" 
 
+# Update to a Rust image 
 # kn service update hello-example --image gcr.io/knative-samples/helloworld-rust
 
+# Curl or Browser Rust deployment
+# curl http://hello-example.knative.jamesbuckett.com
+# Should be: "Hello world: Second"
+
 # Split Service
-# kn service update hello-example --traffic hello-example-bqbbr-2=50 --traffic hello-example-nfwgx-3=50
+# kn service update hello-example --traffic hello-example-00003=50 --traffic hello-example-00002=50
+
+# Curl or Browser Both deployment
+# curl http://hello-example.knative.jamesbuckett.com
+# Should be: "Hello Second!" 50%
+# Should be: "Hello world: Second" 50% 
 
 
 else
