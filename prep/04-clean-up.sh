25e5eaa96c18ae31074fb9da0ec6ce0ffb8659bb
jamesbuckett <james.buckett@gmail.com>
2021-09-03 11:20:50 +0800

    More Volume deletes

--- prep/04-clean-up.sh
+++ prep/04-clean-up.sh
@@ -73,6 +73,9 @@ echo " "
 ################################################################################
 doctl compute volume list | awk 'FNR == 2 {print $1}' | xargs doctl compute volume delete -f
 doctl compute volume list | awk 'FNR == 2 {print $1}' | xargs doctl compute volume delete -f
+doctl compute volume list | awk 'FNR == 2 {print $1}' | xargs doctl compute volume delete -f
+doctl compute volume list | awk 'FNR == 2 {print $1}' | xargs doctl compute volume delete -f
+doctl compute volume list | awk 'FNR == 2 {print $1}' | xargs doctl compute volume delete -f
 printf "%s\n" "digital-ocean-droplet volume deleted"
 echo " "
 
