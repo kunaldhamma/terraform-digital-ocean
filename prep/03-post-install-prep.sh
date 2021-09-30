b08860f06c71ce28d136904768832938ac023286
jamesbuckett <james.buckett@gmail.com>
2021-09-30 11:38:53 +0800

    Changed LB names to be more meaniful

--- prep/03-post-install-prep.sh
+++ prep/03-post-install-prep.sh
@@ -34,17 +34,10 @@ set -o errexit
 # The host names, like "demo.jamesbuckett.com" are CNAME records 
 # to the "do.jamesbuckett.com" A record  
 
-# kubectl config set-context --current --namespace=projectcontour
-# INGRESS_LB=(kubectl get service | (grep -i envoy) | (awk 'FNR == 1 {print $4}'))
+CONTOUR_LB=$(kubectl describe ingress ing-demo -n ns-demo | awk '/Address:/{print $2 }')
+export CONTOUR_LB
 
-# Verify that this IP Addess: kubectl get service | grep -i envoy | awk 'FNR == 1 {print $4}'
-# Is the same as this IP Addess: doctl compute load-balancer list | awk 'FNR == 3 {print $2}'
-# This is the External IP Address of the Contour Load Balancer
-
-INGRESS_LB=$(kubectl describe ingress ing-demo -n ns-demo | awk '/Address:/{print $2 }')
-export INGRESS_LB
-
-doctl compute domain records create --record-type A --record-name www --record-data $INGRESS_LB jamesbuckett.com --record-ttl=43200
+doctl compute domain records create --record-type A --record-name www --record-data $CONTOUR_LB jamesbuckett.com --record-ttl=43200
 doctl compute domain records create jamesbuckett.com --record-type CNAME --record-name demo --record-data www. --record-ttl=43200
 doctl compute domain records create jamesbuckett.com --record-type CNAME --record-name loki --record-data www. --record-ttl=43200
 doctl compute domain records create jamesbuckett.com --record-type CNAME --record-name vpa --record-data www. --record-ttl=43200
@@ -54,8 +47,6 @@ doctl compute domain records create jamesbuckett.com --record-type CNAME --recor
 ################################################################################
 # Loki - Export the Public IP address of Loki and display the password to login
 ################################################################################
-# LOKI_LB=$(doctl compute load-balancer list | awk 'FNR == 3 {print $2}')
-# export LOKI_LB
 LOKI_PWD=$(kubectl get secret --namespace ns-loki loki-release-grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo)
 export LOKI_PWD
 
@@ -105,7 +96,7 @@ echo "fs.file-max=500000" >> /etc/sysctl.conf
 OCTANT_LB=$(doctl compute load-balancer list | awk '/digitalocean-loadbalancer/{print $2 }')
 export OCTANT_LB
 
-OCTANT_LB=$(doctl compute droplet list | awk '/digital-ocean-droplet/{print $1 }')
+OCTANT_DROPLET=$(doctl compute droplet list | awk '/digital-ocean-droplet/{print $1 }')
 export OCTANT_DROPLET
 
 doctl compute load-balancer add-droplets $OCTANT_LB --droplet-ids $OCTANT_DROPLET
